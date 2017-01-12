1bf6fd114e1ceba7305b23462f73c71127207f2c
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2017-01-12 00:31:43 -0200

    atualizacao de mascara do campo

--- public/js/Mask.js
+++ public/js/Mask.js
@@ -1,4 +1,30 @@
 jQuery(function($){
-   $("#phone").mask("(99) 99999-9999");
+   $("#txtPhone").mask("(99) 99999-9999");
 });
 
+$(document).ready(function() {
+    $('#txtPhone').blur(function(e) {
+        if (validatePhone('txtPhone')) {
+            $('#spnPhoneStatus').html('Valid');
+            $('#spnPhoneStatus').css('color', 'green');
+            $("#save").removeAttr("disabled");
+
+        }
+        else {
+            $('#spnPhoneStatus').html('Invalid');
+            $('#spnPhoneStatus').css('color', 'red');
+            $('#save').attr("disabled", "disabled");
+        }
+    });
+});
+
+function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    var filter = /^((\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
